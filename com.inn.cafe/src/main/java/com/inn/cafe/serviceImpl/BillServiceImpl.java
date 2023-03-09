70913cf171516f70426259c57b4f9ce80fca4195
Pablo <pablobhz@gmail.com>
2023-03-08 22:25:10 -0300

    Adding PDF

--- com.inn.cafe/src/main/java/com/inn/cafe/serviceImpl/BillServiceImpl.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/serviceImpl/BillServiceImpl.java
@@ -7,9 +7,11 @@ import com.inn.cafe.model.Bill;
 import com.inn.cafe.service.BillService;
 import com.inn.cafe.utils.CafeUtils;
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.FileOutputStream;
 import java.util.Map;
-
+import java.util.stream.Stream;
 
 
 @Slf4j
@@ -64,6 +66,18 @@ public class BillServiceImpl implements BillService {
             table.setWidthPercentage(100);
             addTableHeader(table);
 
+            JSONArray jsonArray =
+                    CafeUtils.getJsonArrayFromString((String)requestMap.get("productDetails"));
+            for(int i=0; i < jsonArray.length(); i++){
+                addRows(table, CafeUtils.getMapFromJson(jsonArray.getString(i)));
+            }
+            document.add(table);
+            Paragraph footer = new Paragraph("Total: " + requestMap.get("totalAmount") + "\n" +
+                    "Thank you for visiting", getFont("Data"));
+            document.add(footer);
+            document.close();
+            return new ResponseEntity<>("{\"uuid\":\"" + fileName + "\"}", HttpStatus.OK);
+
 
             return CafeUtils.getResponseEntity("Required data not found", HttpStatus.BAD_REQUEST);
 
@@ -74,7 +88,28 @@ public class BillServiceImpl implements BillService {
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    private void addRows(PdfPTable table, Map<String, Object> data) {
+        log.info("Inside addRows");
+        table.addCell((String)data.get("name"));
+        table.addCell((String)data.get("category"));
+        table.addCell((String)data.get("quantity"));
+        table.addCell(Double.toString((Double)data.get("price")));
+        table.addCell(Double.toString((Double)data.get("total")));
+    }
+
     private void addTableHeader(PdfPTable table) {
+        log.info("Inside addTableHeader");
+        Stream.of("Name", "Category", "Quantity", "Price", "Sub Total")
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columnTitle));
+                    header.setBackgroundColor(BaseColor.YELLOW);
+                    header.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    header.setVerticalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(header);
+                });
     }
 
     private Font getFont(String type) {
